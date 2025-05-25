1636119aef9de1b616223e54a954e9e36e4ed08d
sk0x <skbyte8@gmail.com>
2025-05-26 04:04:14 +0530

    few updates

--- installation.sh
+++ installation.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 
-# taking input for package manager
-read -p "Yay(1) or paru(2): " name
+# install aur helper (paru)
+cd ~
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
 
-# install yay or paru
-if [$name == "1"]; then
-    # install yay
-    cd ~
-    git clone https://aur.archlinux.org/yay.git
-    cd yay
-    makepkg -si
-else
-    # install paru
-    cd ~
-    git clone https://aur.archlinux.org/paru.git
-    cd paru
-    makepkg -si
-fi
+# remove folder
+cd ~
+rm -r paru
 
-# installing tools
-sudo pacman -S stow zsh fzf tmux network-manager-applet waybar wlogout hyprpaper nvim
-yay -S hyprshot
+# installing apps 
+sudo pacman -S stow zsh fzf tmux network-manager-applet waybar hyprpaper neovim rofi
+paru -S hyprshot wlogout
+
+# Change shell to zsh
+chsh -s $(which zsh)
+
+# installing ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install browser
+paru -S brave-bin google-chrome zen-browser-bin
 
 # install docker
 sudo pacman -S docker
 sudo systemctl start docker.service
-sudo systemctl enalble docker.service
+sudo systemctl enable docker.service
 sudo usermod -aG docker $USER
 
-# install browser
-yay -S brave-bin google-chrome zen-browser-bin
 
